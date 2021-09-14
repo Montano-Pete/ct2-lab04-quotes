3f352167dc529b8adae9eda59177a59d246d8b74
Peter <pete225848@gmail.com>
2021-09-14 09:41:38 -0700

    created Load component

--- src/components/quote/Quote.jsx
+++ src/components/quote/Quote.jsx
@@ -1,19 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Quote = ({name, image, quote}) => (
+const Quote = ({ name, image, quote }) => (
   <figure>
     <img src={image} alt={name} />
-    <caption>
-      {quote}
-    </caption>
+    <caption>{quote}</caption>
   </figure>
 );
 
 Quote.popTypes = {
   name: PropTypes.string.isRequired,
   image: PropTypes.string.isRequired,
-  quote: PropTypes.string.isRequired
+  quote: PropTypes.string.isRequired,
 };
 
 export default Quote;
